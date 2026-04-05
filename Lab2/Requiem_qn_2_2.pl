2a8aad8bc5b6696cfde44a80653414584e98bbce
The-Magni <dinhquanganh1606@gmail.com>
2026-04-05 11:28:13 +0800

    change sort function

--- Lab2/Requiem_qn_2_2.pl
+++ Lab2/Requiem_qn_2_2.pl
@@ -8,13 +8,13 @@ child('prince edward', 4).
 older(X, Y) :- child(X, OX), child(Y, OY), OX < OY.
 succession(X, Y) :- older(X, Y).
 
-ordered_list(L) :-
+succession_line(L) :-
     findall(X, child(X,_), All), % all X such that child(X, _) is true
-    tsort(All, [], L). % call tsort function, All = remaining nodes, [] = accumulator, L = sorted lists
+    tsort(All, L). % call tsort function, All = remaining nodes, [] = accumulator, L = sorted lists
 
-tsort([], Acc, Sorted) :- reverse(Acc, Sorted). % base case: no node left to sort, reverse the order
-tsort(Nodes, Acc, Sorted) :-
-    % pick a node with no predecessors in Nodes
-    select(Node, Nodes, Rest), % pick 1 node from Nodes, Rest = remaining node after removing
-    \+ (member(Other, Nodes), succession(Other, Node)),
-    tsort(Rest, [Node|Acc], Sorted).
+% topological sort
+tsort([], []). % base case
+tsort(Nodes, [Node|Sorted]) :-
+  select(Node, Nodes, Rest),
+  \+ (member(Other, Nodes), succession(Other, Node)),
+  tsort(Rest, Sorted).
