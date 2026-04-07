4cd9e87c008b547a998f6531eb1c55db340caff1
Regulus128 <nguyenletam1282006@gmail.com>
2026-04-07 21:23:24 +0800

    update

--- Lab2/Requiem_qn_2_2.pl
+++ Lab2/Requiem_qn_2_2.pl
@@ -1,10 +1,10 @@
-% Fact
+% Facts
 child('prince charles', 1).
 child('princess ann', 2).
 child('prince andrew', 3).
 child('prince edward', 4).
 
-% Rule
+% Rules
 older(X, Y) :- child(X, OX), child(Y, OY), OX < OY.
 succession(X, Y) :- older(X, Y).
 
